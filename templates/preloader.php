c2362217e9d2b333fe709fe47a0f926bf5a10d92
James <james.auble@gmail.com>
2021-03-01 11:29:07 -0400

    removed cr templates

--- templates/preloader.php
+++
@@ -1,8 +0,0 @@
-<div id="preloader" style="display: none;">
-  <div id="spinner">
-    <div class="preloader-dot-loading">
-      <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
-    </div>
-  </div>
-  <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
-</div>
