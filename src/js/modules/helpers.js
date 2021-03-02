9b603820bf30eff3a55a1747b4955cd5a5a58a24
James <james.auble@gmail.com>
2021-03-02 16:19:33 -0400

    cleaned up js dir

--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -1,18 +1,4 @@
 exports.init = () => {
-  console.log( 'helpers is initialized!' );
 }
 
-exports.isOverflowRight = ($el) => {
-  const width = $el.outerWidth();
-  const windowWidth = $(window).width();
-  const leftOffset = $el.offset().left;
-  const rightOffset = windowWidth - (width + leftOffset);
-
-  if (rightOffset < 0) {
-    return Math.abs(rightOffset);
-  } else {
-    return false;
-  }
-
-};
 
